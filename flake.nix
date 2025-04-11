6798cd83a3c5f52fe42243f63e9bb31051109702
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-04-11 18:51:38 +0200

    basic rust project

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+
+    naersk.url = "github:nix-community/naersk/master";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    naersk,
+    rust-overlay,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        naersk-lib = pkgs.callPackage naersk {};
+
+        buildDeps = with pkgs; [];
+        runtimeDeps = with pkgs; [];
+      in {
+        packages = {
+          default = naersk-lib.buildPackage {
+            nativeBuildInputs = buildDeps;
+            buildInputs = runtimeDeps;
+
+            src = ./.;
+          };
+        };
+
+        devShell = pkgs.mkShell rec {
+          nativeBuildInputs = buildDeps;
+
+          buildInputs = with pkgs;
+            [
+              (rust-bin.stable.latest.default.override {
+                extensions = ["rust-src"];
+              })
+              rust-analyzer-unwrapped
+            ]
+            ++ runtimeDeps;
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
+        };
+      }
+    );
+}
